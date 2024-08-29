db05a7c3d336c52ec661652d3e00e8d8e587cdeb
Evan <ejones46@huskers.unl.edu>
2024-08-29 10:45:02 -0400

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Evan Jones
+ * Date: 2024/08/28
+ *
+ * A simple program that states my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Evan Jones\n");
+
+}
